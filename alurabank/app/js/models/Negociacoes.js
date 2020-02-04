cea75bb5aadd1f8739f95bedeada12a279540d09
Paulo Henrique Pereira <ph@smi61ti.lan>
2020-02-04 15:05:35 -0300

    TypeScript Definitions, usando JQuery

--- alurabank/app/js/models/Negociacoes.js
+++ alurabank/app/js/models/Negociacoes.js
@@ -1,9 +1,7 @@
 class Negociacoes {
     constructor() {
-        // private _negociacoes: Array<Negociacao> = []; /* É a mesma coisa. */
         this._negociacoes = [];
     }
-    //É uma boa prática tipar os métodos de acordo com seus retornos
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
     }
--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,25 +1,24 @@
 class NegociacaoController {
-    private _inputData: HTMLInputElement;
-    private _inputQuantidade: HTMLInputElement;
-    private _inputValor: HTMLInputElement;
+    private _inputData: JQuery;
+    private _inputQuantidade: JQuery;
+    private _inputValor: JQuery;
     private _negociacoes = new Negociacoes;
     private _negociacoesView = new NegociacoesViews('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView')
 
     constructor() {
-        this._inputData = <HTMLInputElement>document.querySelector('#data');
-        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
-        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
         this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event) {
         event.preventDefault();
         const negociacao = new Negociacao(
-            // new Date(this._inputData.value.replace(/-/g , ',')), /* Aparentemente nessa versão não precisa do replace */
-            new Date(this._inputData.value),
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value)
+            new Date(this._inputData.val().toString()), /* Com JQuery jquery atualizado, é necessário usar o toString para funcionar */
+            Number(this._inputQuantidade.val()),
+            Number(this._inputValor.val())
         );
 
         this._negociacoes.adiciona(negociacao);
