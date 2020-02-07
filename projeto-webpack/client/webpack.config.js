f29ea838aa3282fce887fd3dc0122ba9a2e5a606
Paulo Henrique Pereira <phpereira@outlook.com>
2020-02-07 13:05:52 -0300

    Aula 06 - Boas Práticas, scope hoisting, CommonsChunkPlugin, index.html automatizado, code splitting, lazy loading, System.import() e import, distibuição e endereço da api.

--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -3,8 +3,20 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let plugins = [];
+
+plugins.push(new HtmlWebpackPlugin({
+    hash: true,
+    minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true
+    },
+    filename: 'index.html',
+    template: __dirname + '/main.html'
+}));
 
 plugins.push(
     new MiniCssExtractPlugin(
@@ -17,7 +29,14 @@ plugins.push(new webpack.ProvidePlugin({
     'jQuery': 'jquery/dist/jquery.js'
 }));
 
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
+
 if (process.env.NODE_ENV == 'production') {
+
+    let SERVICE_URL = JSON.stringify('http://endereco-da-sua-api')
+    
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+
     plugins.push(new MinifyPlugin());
 
     plugins.push(new OptimizeCSSAssetsPlugin({
@@ -31,13 +50,14 @@ if (process.env.NODE_ENV == 'production') {
     }));
 }
 
+plugins.push(new webpack.DefinePlugin({ SERVICE_URL }))
+
 module.exports = {
     mode: 'development',
     entry: './app-src/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
+        path: path.resolve(__dirname, 'dist')
     },
 
     module: {
@@ -58,5 +78,14 @@ module.exports = {
     plugins,
     optimization: {
         minimizer: [new OptimizeCSSAssetsPlugin({})],
+        splitChunks: {
+            cacheGroups: {
+                node_vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
     },
 }
