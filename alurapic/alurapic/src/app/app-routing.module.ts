2050552fbc6ffeb1adf61d04ba64b8332239e20e
Paulo Henrique Pereira <phpereira@outlook.com>
2020-02-28 12:01:47 -0300

    Validador assíncrono, submit dos dados

--- alurapic/alurapic/src/app/app-routing.module.ts
+++ alurapic/alurapic/src/app/app-routing.module.ts
@@ -6,19 +6,24 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { SignInComponent } from './home/signin/signin.component';
 import { AuthGuard } from './core/auth/auth.guard';
+import { SignUpComponent } from './home/signup/signup.component';
 
 
 const routes: Routes = [
-  { 
-    path: '', 
+  {
+    path: '',
     component: SignInComponent,
     canActivate: [AuthGuard]
-   },
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent,
+  },
   {
     path: 'user/:userName',
     component: PhotoListComponent,
     resolve: {
-      photos: PhotoListResolver 
+      photos: PhotoListResolver
     }
   },
   { path: 'p/add', component: PhotoFormComponent },
